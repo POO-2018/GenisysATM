77bf7969731900be3918ba64827d3e82d3c98b4d
Hector Sabillon <h.hsabillon@gmail.com>
2018-10-31 10:24:15 -0600

    Creación de la clase CustomException que manejará las excepciones dentro de la Aplicación. Clase de Conexión con la base de datos.

---
+++ GenisysATM/GenisysATM/Models/Conexion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Agregar los namespaces necesarios
+using System.Data;
+using System.Data.SqlClient;
+
+namespace GenisysATM.Models
+{
+    class Conexion
+    {
+        // Propiedades
+        private string servidor;
+        private string baseDatos;
+        public SqlConnection conn;
+        public SqlCommand cmd;
+
+        // Constructores
+        public Conexion() { }
+
+        public Conexion(string elServidor, string laBaseDatos)
+        {
+            servidor = elServidor;
+            baseDatos = laBaseDatos;
+            EstablecerConexion();
+        }
+
+        // Métodos
+        /// <summary>
+        /// Realiza una conexión al servidor de base de datos.
+        /// Requiere el nombre del servidor más la instancia del mismo.
+        /// Requiere el nombre de la base de datos a inicializar.
+        /// </summary>
+        public void EstablecerConexion()
+        {
+            try
+            {
+                conn = new SqlConnection(@"server = " + servidor +
+                    "integrated security = true; database = " + baseDatos);
+            }
+            catch (Exception)
+            {
+                throw new CustomException("¡Servidor o base de datos no encontrados!");
+            }
+        }
+
+        /// <summary>
+        /// Ejecuta un comando SQL.
+        /// </summary>
+        /// <param name="elComando">El query SQL a ejecutar</param>
+        public void EjecutarComando(string elComando)
+        {
+            cmd = new SqlCommand(elComando, conn);
+        }
+    }
+}
